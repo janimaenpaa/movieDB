44127993673713fd264a161100fcb1002318095a
janimaenpaa <itsmayd@gmail.com>
2020-10-29 13:03:09 +0200

    Initial commit

---
+++ src/test/java/swd20/movieDB/MovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package swd20.movieDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
