98c1cdad649b795672b3c96b3ffc886bcd34b001
janimaenpaa <itsmayd@gmail.com>
2020-10-29 14:07:21 +0200

    Add Movie and Review classes

---
+++ src/main/java/swd20/movieDB/domain/Movie.java
@@ -0,0 +1,92 @@
+package swd20.movieDB.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long movieId;
+	private String title, description, imgUrl;
+	private int publicationYear;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "movie")
+	@JsonIgnoreProperties("movie")
+	private List<Review> reviews;
+
+	public Movie() {
+	}
+
+	public Movie(long id, String title, int publicationYear, String description, String imgUrl) {
+		super();
+		this.movieId = id;
+		this.title = title;
+		this.publicationYear = publicationYear;
+		this.description = description;
+		this.imgUrl = imgUrl;
+	}
+
+	public long getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(long movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [movieId=" + movieId + ", title=" + title + ", description=" + description + ", imgUrl=" + imgUrl
+				+ ", publicationYear=" + publicationYear + "]";
+	}
+
+}
---
+++ src/main/java/swd20/movieDB/domain/Review.java
@@ -0,0 +1,91 @@
+package swd20.movieDB.domain;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class Review {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String reviewer, description;
+	private Date date;
+	private int rating;
+	@ManyToOne
+	@JsonIgnoreProperties("reviews")
+	@JoinColumn(name = "movieId")
+	private Movie movie;
+
+	public Review() {
+	}
+
+	public Review(long id, String reviewer, String description, Date date, int rating, Movie movie) {
+		super();
+		this.id = id;
+		this.reviewer = reviewer;
+		this.description = description;
+		this.date = date;
+		this.rating = rating;
+		this.movie = movie;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getReviewer() {
+		return reviewer;
+	}
+
+	public void setReviewer(String reviewer) {
+		this.reviewer = reviewer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", reviewer=" + reviewer + ", description=" + description + ", date=" + date + "]";
+	}
+
+}
