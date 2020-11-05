96422026bbf9b5c44e8a0e56a2d27682a5ef14b6
janimaenpaa <itsmayd@gmail.com>
2020-11-05 14:31:50 +0200

    Add Movie page

--- src/main/java/swd20/movieDB/web/MovieController.java
+++ src/main/java/swd20/movieDB/web/MovieController.java
@@ -1,9 +1,12 @@
 package swd20.movieDB.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import swd20.movieDB.domain.Movie;
@@ -20,13 +23,24 @@ public class MovieController {
 		model.addAttribute("movies", movieRepository.findAll());
 		return "movies";
 	}
-	
+
+	@GetMapping("/movies/{id}")
+	public String moviePage(@PathVariable("id") long movieId, Model model) {
+		Optional<Movie> movie = movieRepository.findById(movieId);
+
+		if (movie != null) {
+			model.addAttribute("movie", movie.get());
+		}
+
+		return "movie";
+	}
+
 	@GetMapping("/movies/new")
 	public String newMovie(Model model) {
 		model.addAttribute("movie", new Movie());
 		return "newmovie";
 	}
-	
+
 	@PostMapping("/movies/save")
 	public String saveMovie(Movie movie) {
 		movieRepository.save(movie);
