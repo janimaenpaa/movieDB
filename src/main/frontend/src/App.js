8fdbf3baff71d9a8021ad803066eed651fbbfe0e
janimaenpaa <itsmayd@gmail.com>
2020-11-11 12:49:34 +0200

    Add React Routing

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 const App = () => {
-  return <div>tiikeri</div>
+  return (
+    <Router>
+      <div>
+        <Link to="/movie/1">Movie</Link>
+      </div>
+      <Switch>
+        <Route path="/movie/:id">MoviePage</Route>
+        <Route path="/">MovieList</Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
