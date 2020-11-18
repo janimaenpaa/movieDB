c0b05d6b31f9e8ce609a986f2c5328a3b40e547e
janimaenpaa <itsmayd@gmail.com>
2020-11-19 01:02:54 +0200

    Add Admin button to NavBar

--- src/main/frontend/src/components/AppBar.js
+++ src/main/frontend/src/components/AppBar.js
@@ -1,15 +1,20 @@
 import React from "react"
-import { Navbar } from "react-bootstrap"
+import { Button, Navbar } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 const AppBar = () => {
   return (
-    <Navbar bg="dark" variant="dark mb-4">
+    <Navbar bg="dark" expand="true" variant="dark mb-4">
       <Navbar.Brand className="d-inline-block align-top">
         <Link style={{ color: "#fff", textDecoration: "none" }} to="/">
           MovieDB
         </Link>
       </Navbar.Brand>
+      <Button className="d-inline-block align-top" variant="secondary">
+        <a style={{ color: "#fff", textDecoration: "none" }} href="#">
+          Admin
+        </a>
+      </Button>
     </Navbar>
   )
 }
