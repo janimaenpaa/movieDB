dca2a802336bf213836d979460ff9e4cf386baaa
janimaenpaa <itsmayd@gmail.com>
2020-11-19 00:19:55 +0200

    Refactor components

---
+++ src/main/frontend/src/MovieList/MovieCard.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { Badge, Card } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import { averageRating } from "../utils"
+
+const MovieCard = ({ movie }) => {
+  const rating = averageRating(movie.reviews)
+  return (
+    <Card style={{ flex: 1, minWidth: "30%", maxWidth: "30%", margin: 10 }}>
+      <Link style={{ textDecoration: "none" }} to={`/movies/${movie.movieId}`}>
+        <Card.Img src={movie.imgUrl} alt="Poster" />
+      </Link>
+      <Card.Body className="d-flex justify-content-between">
+        <div>
+          <Link
+            style={{ textDecoration: "none" }}
+            to={`/movies/${movie.movieId}`}
+          >
+            <Card.Title>{movie.title}</Card.Title>
+          </Link>
+          <Card.Subtitle className="text-muted">
+            {movie.publicationYear}
+          </Card.Subtitle>
+        </div>
+        <Card.Text
+          style={{ fontSize: 20 }}
+          className="d-flex align-items-center"
+        >
+          {rating ? (
+            <Badge pill variant="warning p-2">{`${rating} / 5.0`}</Badge>
+          ) : (
+            <Badge pill variant="warning p-2">
+              No rating
+            </Badge>
+          )}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default MovieCard
