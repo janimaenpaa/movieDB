246bee97b668902fa8249eafebcfcc057cd6e5ba
janimaenpaa <itsmayd@gmail.com>
2020-11-19 08:21:34 +0200

    Add security to / end point

--- src/main/java/swd20/movieDB/WebSecurityConfig.java
+++ src/main/java/swd20/movieDB/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**", "/public", "/", "/api/**").permitAll().and().authorizeRequests()
+		http.authorizeRequests().antMatchers("/css/**", "/public", "/api/**").permitAll().and().authorizeRequests()
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll()
 				.and().logout().permitAll();
 	}
